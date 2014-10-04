e6f34976955fad70b3b622e3958e0b9c1d1b886d
pieszynski <git@pieszynski.com>
2014-10-05 00:30:17 +0200

    +1

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ var express = require('express');
 
 var app = module.exports = express();
 
+app.use(express.compress());
 
 app.use('/static', express.static(__dirname + '/static'));
 
 app.get('/', function(req,res){
-    res.send('Hello from express +1');
+    res.send('Hello from express +1\n<img src="/static/PIA.jpg"/>');
 });
 
 app.listen(4080, function() {

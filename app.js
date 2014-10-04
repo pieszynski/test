f1624353db33fd59d89109bc4cd8124c35830086
pieszynski <git@pieszynski.com>
2014-10-04 23:18:26 +0200

    +1

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ var express = require('express');
 
 var app = module.exports = express();
 
-app.get('/', function(req,res){
+app.use(/^\/api\//i, function(req,res){
+    res.send('Hello API');
+})
+
+app.get(function(req,res){
     res.send('Hello from express +1');
 });
 
-app.listen(4080, 'vps.pieszynski.com' ,function() {
+app.listen(4080, function() {
     console.log('Server running at http://*:4080/');
 });

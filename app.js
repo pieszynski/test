8e8077dfddbc1c8bbf9c26408f1d2eabc1fe587a
pieszynski <git@pieszynski.com>
2014-10-05 00:19:50 +0200

    +1

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = module.exports = express();
 
 
-app.use('/static', express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '/static'));
 
 /*app.get('/', function(req,res){
     res.send('Hello from express +1');

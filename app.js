94cd5789d49b2fbde72f6a30b18413b7a5191afd
pieszynski <git@pieszynski.com>
2014-10-04 14:47:47 +0200

    Change name to app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-    res.end('Witaj gościu! Ponownie?');
+    res.end('Can i haz app.js host?');
 }).listen(4080);
 console.log('Server running at http://*:4080/');

ac5e428589c23389e08aeb9747562a4bfa295554
pieszynski <git@pieszynski.com>
2014-10-04 15:16:40 +0200

    +1

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-    res.end('Can i haz app.js host another commit?');
+    res.end('Can i haz app.js host another one or probably?');
 }).listen(4080);
 console.log('Server running at http://*:4080/');

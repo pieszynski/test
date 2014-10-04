fe989ccfd91843595063d3450aa102814354ab93
pieszynski <git@pieszynski.com>
2014-10-04 16:54:32 +0200

    bold move to express

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-var http = require('http');
+//var http = require('http');
+var express = require('express');
 
-http.createServer(function (req, res) {
+var app = module.exports = express.createServer();
+
+/*http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
     res.end('Can i haz app.js host another one or probably?');
-}).listen(4080);
+}).listen(4080);*/
+
+app.get('/', function(req,res){
+    res.send('Hello from express');
+});
+
+app.listen(4000);
 console.log('Server running at http://*:4080/');

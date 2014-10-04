5819870f8a8a3625a689016a49607efaad079c2a
pieszynski <git@pieszynski.com>
2014-10-05 00:24:32 +0200

    +1

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var express = require('express');
 var app = module.exports = express();
 
 
-app.use(express.static(__dirname + '/static'));
+app.use('/static', express.static(__dirname + '/static'));
 
-/*app.get('/', function(req,res){
+app.get('/', function(req,res){
     res.send('Hello from express +1');
-});*/
+});
 
 app.listen(4080, function() {
     console.log('Server running at http://*:4080/');

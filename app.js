dfee19d15178d0b0c0fe7dfa069631ccdc2f8173
pieszynski <git@pieszynski.com>
2014-10-05 16:34:13 +0200

    +2

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ var compression = require('compression')
 var express = require('express');
 
 var app = module.exports = express();
+var router = express.Router();
 
 app.use(compression());
 
-app.use('/static', express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '/app'));
+app.use('/bootstrap', express.static(__dirname + '/bower_components/bootstrap/dist'));
+app.use('/angular', express.static(__dirname + '/bower_components/angular'));
+app.use('/jquery', express.static(__dirname + '/bower_components/jquery/dist'));
 
-app.get('/', function(req,res){
-    res.send('<h1>Hello from express +1</h1><div><img src="/static/PIA.jpg"/></div>');
+app.get(/.*/i, function(req,res){
+    //res.send('<h1>Hello from express +1</h1><div><img src="/img/PIA.jpg"/></div>');
+    res.redirect('/error/#404');
 });
 
 app.listen(4080, function() {
